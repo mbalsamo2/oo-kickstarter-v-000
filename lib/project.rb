e8478d1805efd33da9ce9377a2e1dccec8b9a33c
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-09 22:09:08 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    self.title = title
+    self.backers = []
+  end
+
+  def add_backer(backer)
+    self.backers << backer
+    backer.backed_projects << self
+  end
+
+end
